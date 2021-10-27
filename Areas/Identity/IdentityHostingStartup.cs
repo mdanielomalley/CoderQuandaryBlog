a1962830fa17ca7ed2d4f4649b04558f9b95bc12
Crom <m.daniel.omalley@gmail.com>
2021-10-27 19:05:55 -0400

    Added Identity Scaffolding

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CoderQuandaryBlog.Data;
+using CoderQuandaryBlog.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CoderQuandaryBlog.Areas.Identity.IdentityHostingStartup))]
+namespace CoderQuandaryBlog.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
